921ab868dda56b5abc8e13d436300e240c65efc4
kosyachniy <polozhev@mail.ru>
2019-08-08 04:10:52 +0500

    Tasks

--- api/app/api.py
+++ api/app/api.py
@@ -3,25 +3,36 @@ from app import app
 
 import requests
 
-from sets import LINK, CODE, SECRET
+from keys import LINK, CLIENT_ID, CLIENT_SECRET, TOKEN
 
 
 @app.route('/')
 def auth():
-	return '<a href="https://www.wrike.com/oauth2/authorize/v4?client_id={}&response_type=code&redirect_uri={}login">Authorize</a>'.format(CODE, LINK)
+	return '<a href="https://www.wrike.com/oauth2/authorize/v4?client_id={}&response_type=code&redirect_uri={}login">Authorize</a>'.format(CLIENT_ID, LINK)
 
 @app.route('/login')
 def token():
 	code = request.args.get('code')
 
 	data = {
-		'client_id': CODE,
-		'client_secret': SECRET,
+		'client_id': CLIENT_ID,
+		'client_secret': CLIENT_SECRET,
 		'grant_type': 'authorization_code',
 		'code': code,
-		'redirect_uri': LINK + 'qwe',
 	}
 
 	cont = requests.post('https://www.wrike.com/oauth2/token', data=data)
 
-	return jsonify({'cont': cont.text})
+	return cont.text
+
+@app.route('/tasks')
+def tasks():
+	headers = {
+		'Authorization': 'bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjI0OTg5MzIsXCJpXCI6NjQ0ODQxNyxcImNcIjo0NjEzMDA2LFwidVwiOjY0Mzc4MzIsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkFcIixcIkxcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE1NjUxOTAxMTN9.wZA1aP04twbWNKcsYkdhZvNdBHnwLcYpIFj8VCNx-B4',
+	}
+
+	params = {}
+
+	cont = requests.get('https://www.wrike.com/api/v4/tasks', headers=headers, params=params)
+
+	return cont.text
--- api/sets.py
+++
@@ -1,3 +0,0 @@
-LINK = 'http://127.0.0.1:5000/'
-CODE = 'AR1fNunM'
-SECRET = 'G2LJpOvjqNyMyLzrYDpvy75YpxBNRLrTy802QmB6MhYfnupV7rDLSl883SzY3wxL'
