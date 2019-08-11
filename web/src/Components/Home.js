dfb7f9cc1bce4e59594f58e504a3ad5fc81208be
mike-petrov <mis5116@yandex.ru>
2019-08-11 15:18:09 +0300

    new elem on main page

--- web/src/Components/Home.js
+++ web/src/Components/Home.js
@@ -11,34 +11,47 @@ class Home extends React.Component {
                     <div className="card">
                         <div className="card_title">Publish our extension</div>
                         <div className="card_contacts">
-                            <span className="photo"><img src="./img/savva.png" /></span>
+                            <span className="photo"><img src="/img/savva.png" /></span>
                             <span className="date">12 Aug</span>
-                            <span><a className="injected_btn" href="#download" draggable="false">Discuss</a></span>
+                            <span><a className="injected_btn" href="#solution" draggable="false">Discuss</a></span>
                         </div>
                     </div>
-                    <div className="text">Use the only one <span className="gradient gradient_green">Wrike account</span> with all the neccessary tools for <span className="gradient gradient_hot">efficient</span> team work</div>
+                    <div className="text"><span className="gradient gradient_green">Discuss</span> tasks right on the <span className="gradient gradient_hot">wrike</span> platfrom</div>
+                </div>
+                <div className="container" id="solution">
+                    <div className="title">Problem and Our Solution</div>
+                    <div className="block_flex">
+                        <div className="block_content">
+                            <img src="/img/picture_before.png" />
+                            <div className="text"><span className="gradient gradient_hot">STOP</span> wasting time on multiple talks around your tasks</div>
+                        </div>
+                        <div className="block_content">
+                            <img src="/img/picture_after.png" />
+                            <div className="text"><span className="gradient gradient_green">USE</span> exTensy to discuss the tasks right on the wrike platform and get them done faster</div>
+                        </div>
+                    </div>
                 </div>
                 <div className="container" id="download">
-                    <div className="title">Steps to success:</div>
+                    <div className="title">Steps to success</div>
                     <div className="screenshots">
                         <div className="screenshot">
-                            <img src="./img/1.png" />
+                            <img src="/img/picture_before.png" />
                             <div className="screenshot_title">1. Download Extensy and switch it on</div>
                         </div>
                         <div className="screenshot">
-                            <img src="./img/1.png" />
+                            <img src="/img/picture_before.png" />
                             <div className="screenshot_title">2. Choose any task on the Wrike platfrom</div>
                         </div>
                         <div className="screenshot">
-                            <img src="./img/1.png" />
+                            <img src="/img/picture_before.png" />
                             <div className="screenshot_title">3. Discuss the card with your teammate and edit it right during the call</div>
                         </div>
                     </div>
                 </div>
                 <div className="hero_footer">
-                    <div className="text">Make your team more productive!</div>
+                    <div className="text">Make your team more <span className="gradient gradient_green">productive</span>!</div>
                     <div className="btn_block">
-                        <a href="/" className="injected_btn">TRY NOW</a>
+                        <a href="/extension.zip" download="" className="injected_btn">Download</a>
                     </div>
                 </div>
                 <div className="footer">
